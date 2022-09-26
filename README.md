eb11c338e091ded76f425336d227911c1ea30e8d
Robert Cole <1655805+robhasacamera@users.noreply.github.com>
2022-09-25 18:25:47 -0700

    Flow layout (#1)
    * Working on flow layout
    
    * Fixed some of the issues with flow layout
    
    * Improving flow layout preview
    
    * Adding some documentation.
    
    * Updating Readme with installation instruction and future plans
    
    * Adjusting CI builds to run on latest
    
    * Updating CI to use latest version of macOS
    
    * Updating CI
    
    * Removing OS requirements from CI for now.
    
    * Adding xcode select to CI
    
    * Trying to fix CI builds
    
    * Trying to fix CI builds
    
    * Making flow layout iOS only to fix CI.

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 [![package build workflow](https://github.com/robhasacamera/CrystalViewUtilities/actions/workflows/mac-package.yml/badge.svg)](https://github.com/robhasacamera/CrystalViewUtilities/actions/workflows/mac-package.yml)
 
 Contains useful extensions and views for working with SwiftUI.
+
+## Installation
+
+CrystalViewUtilities supports Swift Package Manager. To use it the following to your `Package.swift` file:
+
+```
+dependencies: [
+    .package(name: "CrystalViewUtilities", url: "https://github.com/robhasacamera/CrystalViewUtilities.git", from: "0.0.1")
+],
+```
+
+## Future Plans
+
+This package will be added to as more useful utilities are discovered while building Cyrstal UI.
---
+++ Sources/CrystalViewUtilities/Layout/CUIFlowLayout.swift
@@ -0,0 +1,251 @@
+//
+// CrystalViewUtilities
+//
+// MIT License
+//
+// Copyright (c) 2022 Robert Cole
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+import SwiftUI
+
+// FIXME: Have not found a way to select macOS 12.6 for CI. Will address in future release
+#if os(iOS)
+/**
+ * Created a layout that flows from one row to the next.
+ *
+ * Rows are sized to fit as many elements as possible horizontally before wrapping to the next line.
+ * Rows will be as tall as their tallest element and elements are centered aligned in the row.
+ */
+@available(iOS 16, macOS 12.6, *)
+public struct CUIFlowLayout: Layout {
+    let horizontalSpacing: CGFloat
+    let verticalSpacing: CGFloat
+
+    // TODO: Add alignment options for row (center, top, bottom)
+    // TODO: Add alignment options for layout as a whole (leading, center, trailing), this will probably be much harder
+    /// Creates a flow layout using the provided spacing.
+    /// - Parameters:
+    ///   - horizontalSpacing: Spacing between elements in the row.
+    ///   - verticalSpacing: Spacing between rows
+    public init(
+        horizontalSpacing: CGFloat = .standardSpacing,
+        verticalSpacing: CGFloat = .standardSpacing
+    ) {
+        self.horizontalSpacing = horizontalSpacing
+        self.verticalSpacing = verticalSpacing
+    }
+
+    func rows(
+        subviews: Subviews,
+        proposal: ProposedViewSize
+    ) -> (rows: [[LayoutSubview]], width: CGFloat) {
+        var maxWidth: CGFloat = 0
+
+        // For special cases, it'll place one item per row.
+        if let width = proposal.width,
+           proposal != .zero,
+           proposal != .infinity,
+           proposal != .unspecified
+        {
+            maxWidth = width
+        }
+
+        var rows: [[LayoutSubview]] = []
+
+        var width: CGFloat = 0
+        var currentRowWidth: CGFloat = 0
+
+        for subview in subviews {
+            if currentRowWidth > maxWidth {
+                currentRowWidth = 0
+            }
+
+            // This will be true for the first run as well.
+            if currentRowWidth == 0 {
+                rows.append([])
+            }
+
+            guard var currentRow = rows.last else {
+                // This should never happen
+                break
+            }
+
+            if currentRowWidth > 0 {
+                currentRowWidth += horizontalSpacing
+            }
+
+            let subviewWidth = subview.dimensions(in: proposal).width
+
+            currentRowWidth += subviewWidth
+
+            if currentRowWidth > maxWidth {
+                rows.append([subview])
+                currentRowWidth = subviewWidth
+            } else {
+                currentRow.append(subview)
+                rows[rows.count - 1] = currentRow
+            }
+
+            if currentRowWidth > width {
+                width = currentRowWidth
+            }
+        }
+
+        return (rows: rows, width: width)
+    }
+
+    // TODO: Handle vertical layouts as well
+    public func sizeThatFits(
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout ()
+    ) -> CGSize {
+        let (rows, actualWidth) = rows(subviews: subviews, proposal: proposal)
+
+        var actualHeight: CGFloat = 0
+
+        for row in rows {
+            let rowHeight = row.maxHeight(in: proposal)
+
+            if actualHeight > 0 {
+                actualHeight += verticalSpacing
+            }
+
+            actualHeight += rowHeight
+        }
+
+        return CGSize(width: actualWidth, height: actualHeight)
+    }
+
+    public func placeSubviews(
+        in bounds: CGRect,
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout ()
+    ) {
+        let (rows, _) = rows(subviews: subviews, proposal: proposal)
+
+        var y: CGFloat = 0
+        var x: CGFloat = 0
+
+        for row in rows {
+            x = 0
+
+            let rowHeight = row.maxHeight(in: proposal)
+
+            if y > 0 {
+                y += verticalSpacing
+            }
+
+            y += rowHeight / 2
+
+            for subview in row {
+                if x > 0 {
+                    x += horizontalSpacing
+                }
+
+                let subviewWidth = subview.dimensions(in: proposal).width
+
+                x += subviewWidth / 2
+
+                subview.place(
+                    at: CGPoint(
+                        x: x + bounds.origin.x,
+                        y: y + bounds.origin.y
+                    ),
+                    anchor: .center,
+                    proposal: ProposedViewSize(
+                        width: subviewWidth,
+                        height: rowHeight
+                    )
+                )
+
+                x += subviewWidth / 2
+            }
+
+            y += rowHeight / 2
+        }
+    }
+}
+
+@available(iOS 16, macOS 12.6, *)
+private extension Array where Element == LayoutSubview {
+    func maxHeight(in proposal: ProposedViewSize) -> CGFloat {
+        var height: CGFloat = 0
+
+        for subview in self {
+            let subviewHeight = subview.dimensions(in: proposal).height
+
+            if subviewHeight > height {
+                height = subviewHeight
+            }
+        }
+
+        return height
+    }
+
+    func maxWidth(in proposal: ProposedViewSize) -> CGFloat {
+        var width: CGFloat = 0
+
+        for subview in self {
+            let subviewWidth = subview.dimensions(in: proposal).width
+
+            if subviewWidth > width {
+                width = subviewWidth
+            }
+        }
+
+        return width
+    }
+}
+
+struct CUIFlowLayout_Previews: PreviewProvider {
+    static func text(for index: Int) -> String {
+        if index % 7 == 0{
+            return "wider \(index)"
+        } else if index % 9 == 0 {
+            return "really\ntall\n\(index)"
+        } else if index % 3 == 0 {
+            return "taller\n\(index)"
+        }
+
+        return "\(index)"
+    }
+
+    static var previews: some View {
+        if #available(iOS 16, macOS 12.6, *) {
+            return CUIFlowLayout {
+                ForEach(0 ..< 50, id: \.self) { index in
+                    Text(text(for: index))
+                        .padding(.standardSpacing)
+                        .background(.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: .cornerRadius))
+                }
+            }
+            .padding()
+            .background(.gray.opacity(0.5))
+            .clipShape(RoundedRectangle(cornerRadius: .cornerRadius))
+        } else {
+            return EmptyView()
+        }
+    }
+}
+#endif
