a22d7125e461c3d395b808abcbe604cc37366108
Robert Cole <robhasacamera@gmail.com>
2023-02-21 12:52:13 -0500

    Fixing a truncated comment.

---
+++ Sources/CrystalViewUtilities/SampleApp/CVUDemoApp/CVUDemoApp/CVUDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVUDemoAppApp.swift
+//  CVUDemoApp
+//
+//  Created by Robert Cole on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CVUDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sources/CrystalViewUtilities/SampleApp/CVUDemoApp/CVUDemoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CVUDemoApp
+//
+//  Created by Robert Cole on 12/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
